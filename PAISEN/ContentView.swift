6580c6c1e3d5f790026a1b0c17ee4475edbafc46
NagamineHiroki <weezy.23633@gmail.com>
2021-02-19 09:01:50 +0900

    てst

---
+++ PAISEN/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  PAISEN
+//
+//  Created by 永峯大暉 on 2021/02/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome to PAISEN!")
+            .font(.largeTitle)
+            .padding()
+    }
+}
---
+++ PAISEN/PAISENApp.swift
@@ -0,0 +1,17 @@
+//
+//  PAISENApp.swift
+//  PAISEN
+//
+//  Created by 永峯大暉 on 2021/02/14.
+//
+
+import SwiftUI
+
+@main
+struct PAISENApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
